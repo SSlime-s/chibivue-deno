d0ceedf2d981524cea9d960befdfc18ec51badd4
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2024-10-08 12:12:34 +0900

    ✨ dist を消す

--- examples/playground/dist/main.js
+++
@@ -1,7 +0,0 @@
-// home/shumai/misc/chibivue-deno/packages/mod.ts
-var helloChibivue = () => {
-  console.log("Hello chibivue!");
-};
-
-// home/shumai/misc/chibivue-deno/examples/playground/main.ts
-helloChibivue();
