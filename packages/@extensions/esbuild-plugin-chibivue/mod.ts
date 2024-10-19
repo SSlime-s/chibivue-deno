02909d6368f1c3a197b3e6a94ef8a8b5e3e453de
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2024-10-19 10:56:30 +0900

    ✨ css のインポートもできるように

--- packages/@extensions/esbuild-plugin-chibivue/mod.ts
+++ packages/@extensions/esbuild-plugin-chibivue/mod.ts
@@ -5,15 +5,45 @@ import { basename } from "node:path";
 import { compile } from "../../compiler-dom/mod.ts";
 import { rewriteDefault } from "../../compiler-sfc/rewriteDefault.ts";
 
+const VIRTUAL_CSS_NAMESPACE = "chibiVueSFCCss";
+
 export default function esbuildPluginChibivue(): Plugin {
   return {
     name: "esbuild:chibivue",
     setup(build) {
+      build.onResolve({ filter: /\.vue\.css$/ }, (args) => {
+        return {
+          path: args.path,
+          namespace: VIRTUAL_CSS_NAMESPACE,
+        };
+      });
+      build.onLoad(
+        { filter: /\.vue\.css$/, namespace: VIRTUAL_CSS_NAMESPACE },
+        async (args) => {
+          console.log(args.path);
+          const vueFileName = args.path.replace(/\.css$/, "");
+          const contents = await Deno.readTextFile(vueFileName);
+
+          const { descriptor } = parse(contents, {
+            filename: basename(vueFileName),
+          });
+
+          const styles = descriptor.styles.map((style) => style.content).join(
+            "\n",
+          );
+
+          return {
+            contents: styles,
+            loader: "css",
+          };
+        },
+      );
       build.onLoad({ filter: /\.vue$/ }, async (args) => {
         const contents = await Deno.readTextFile(args.path);
 
         const outputs: string[] = [];
         outputs.push("import * as ChibiVue from 'chibivue'\n");
+        outputs.push(`import "${args.path}.css"\n`);
 
         const { descriptor } = parse(contents, {
           filename: basename(args.path),
